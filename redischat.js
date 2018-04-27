26021d92c83cdb42cc07b0b1b1bc8b35896767b1
Hailong Yang <hyang@ESHIPGLOBAL.LOCAL>
2018-04-27 14:19:23 -0500

    mongodb

--- redischat.js
+++ redischat.js
@@ -8,5 +8,23 @@ var server = net.createServer(function(socket) {
     socket.on('connect', function() {
        subscriber = redis.createClient();
        subscriber.subscribe('main_chat_room');
+
+       subscriber.on('message', function(channel, message) {
+           socket.write('Channel ' + channel + ': ' + message);
+       });
+
+       publisher = redis.createClient();
+    });
+
+    socket.on('data', function(data) {
+       publisher.publish('main_chat_room', data); 
+    });
+
+    socket.on('end', function() {
+        subscriber.unsubscribe('main_chat_room');
+        subscriber.end();
+        publisher.end();
     });
 });
+
+server.listen(3000);
