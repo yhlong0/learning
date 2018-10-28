ca9e8d0d097bc2a87afdec41d5065bddcc5331a4
yhlong0 <yhlong2@gmail.com>
2018-10-28 17:41:44 -0500

    add function get index

---
+++ DataStructure/Heap.js
@@ -0,0 +1,37 @@
+let heap = [];
+
+function getLeftChildIndex(parentIndex) {
+    return parentIndex + 1;
+}
+
+function getRightChildIndex(parentIndex) {
+    return parentIndex + 2;
+}
+
+function getParentIndex(childIndex) {
+    return (childIndex - 1) / 2;
+}
+
+function hasLeftChild(index) {
+    
+}
+
+
+function hasRightChild(index) {
+    
+}
+
+
+function leftChild(index) {
+    
+}
+
+
+function rightChild(index) {
+    
+}
+
+
+function parent(index) {
+    
+}
