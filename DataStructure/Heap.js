5cfc827dd6fda6c44664c53a0d0aad2211b00515
yhlong0 <yhlong2@gmail.com>
2018-10-28 21:40:18 -0500

    finish heap methods

--- DataStructure/Heap.js
+++ DataStructure/Heap.js
@@ -21,6 +21,9 @@ function hasRightChild(index) {
     return getRightChildIndex(index) < heap.length;
 }
 
+function hasParent(index) {
+    return getParentIndex(index) >= 0;
+}
 
 function leftChild(index) {
     return heap[getLeftChildIndex(index)];
@@ -34,4 +37,52 @@ function rightChild(index) {
 
 function parent(index) {
     return heap[getParentIndex(index)];
+}
+
+function swap(indexOne, indexTwo) {
+    let temp = heap[indexOne];
+    heap[indexOne] = heap[indexTwo];
+    heap[indexTwo] = temp;
+}
+
+function poll() {
+    let heap = heap[0];
+    heap[0] = heap[heap.length - 1];
+    heap.pop();
+    heapifyDown();
+    return heap;
+}
+
+function add(item) {
+    heap.push(item)
+    heapifyUp();
+    return heap;
+}
+
+function heapifyUp() {
+    let index = heap.length - 1;
+    while (hasParent(index) && parent(index) > heap[index]) {
+        swap(getParentIndex(index), index)
+        index = getParentIndex(index);
+    } 
+}
+
+function heapifyDown() {
+    let index = 0;
+    let smallerChildIndex;
+    
+    while (hasLeftChild(index + 1)) {
+        smallerChildIndex = index + 1;
+        if(hasRightChild(index + 2) && rightChild(index + 2) < leftChild(index + 1)) {
+            smallerChildIndex = index + 2;
+        }
+
+        if(heap[index] < heap[smallerChildIndex]) {
+            break;
+        } else {
+            swap(index, smallerChildIndex);
+        }
+
+        index = smallerChildIndex;
+    }
 }
