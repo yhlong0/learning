d15782684fe3317c84dd8e1e34b0558868a9ed2b
yhlong0 <yhlong2@gmail.com>
2018-11-06 10:02:42 -0600

    create array of markers

--- React/IBM-eShipChain-Map/IBM-eShipChain-Map/src/components/MapPage.js
+++ React/IBM-eShipChain-Map/IBM-eShipChain-Map/src/components/MapPage.js
@@ -11,7 +11,7 @@ import plane from '../img/air-freight.png';
 import ship from '../img/cargo-ship.png';
 import truck from '../img/delivery-truck.png';
 import redBox from '../img/redbox.png';
-
+import { extname } from "path";
 
 
 const styles = theme => ({
@@ -20,77 +20,121 @@ const styles = theme => ({
   }
 });
 
-const MyMapComponent = withScriptjs(withGoogleMap((props) =>
-  <GoogleMap
-    defaultZoom={3}
-    defaultCenter={{ lat: 25.062400, lng: 153.042605 }}
-  >
-    <MarkerClusterer
-      averageCenter
-      enableRetinaIcons
-      gridSize={90}
-    >
-      <Marker 
-        position={{ lat: 35.900762, lng: 139.642131 }} 
-        options={{ icon: box }}
-      >
-        <InfoWindow>
-          <div>
+const markers = [
+  {
+    index: 1,
+    lat: 25.062400,
+    lng: 153.042605,
+    icon: box,
+    info: `<div>
             <h1 id="firstHeading" class="firstHeading">Shanghai Pudong International Airport</h1>
             <div id="bodyContent">
               <p><b>Shipment ID:</b> 123456789</p>
               <p><b>Shipment Date:</b> 11/01/2018</p>
-              <p><b>Receiver:</b> John Smith</p> 
+              <p><b>Receiver:</b> John Smith</p>
               <p><b>Tracking Status:</b> At Shanghai airport waiting for customs clearance.</p>
-            </div> 
-          </div>
-        </InfoWindow>
-      </Marker>
-      <Marker position={{lat: 45.848175, lng: -108.387897 }} options={{ icon: blinkBox }} >
-        <InfoWindow>
-          <div>
-            <h1 id="firstHeading" class="firstHeading" style={{color: 'green'}}>Delivered</h1>
+            </div>
+          </div>`
+  },
+  {
+    index: 2,
+    lat: 45.848175,
+    lng: 139.642131,
+    icon: box,
+    info: `<div>
+            <h1 id="firstHeading" class="firstHeading">Shanghai Pudong International Airport</h1>
             <div id="bodyContent">
-              <p><b>Shipment ID:</b> 5421589900</p>
-              <p><b>Delivery Date:</b> 11/02/2018</p>
-              <p><b>Destination:</b> Texarkana, AR US</p>
-              <p><b>Receiver:</b> Alice Smith</p> 
-              <p><b>Tracking Status:</b></p> 
-              <p> 12/29 10:45 am Delivered <b>New Delhi, India</b></p>
-              <p> 12/29 8:45 am on FedEx vehicle for delivery <b>New Delhi, India</b></p> 
-              <p> 12/29 7:10 am at local FedEx facility <b>New Delhi, India</b></p>
-              <p> 12/28 5:30 am Departed FedEx location <b>New Delhi, India</b></p>
-              <p> 12/28 7:45 am Arrived at FedEx location <b>Los Angeles, CA</b></p>
-              <p> 12/27 7:20 am Picked up <b>Houston, TX</b> </p>
-              <p> 12/26 7:36am at FedEx origin facility <b>Houston, TX</b> </p> 
-            </div> 
-          </div>
-        </InfoWindow>
-      </Marker>
-      <Marker position={{lat: 19.192271, lng: -99.373728 }} options={{ icon: plane }} />
-      <Marker position={{lat: 17.192271, lng: 83.373728 }} options={{ icon: ship }} />
-      <Marker position={{lat: 34.679271, lng: 135.479528 }} options={{ icon: truck }} />
-      <Marker position={{lat: 24.679271, lng: 125.479528 }} options={{ icon: redBox }} />
-    </MarkerClusterer>
-  </GoogleMap>
-));
+              <p><b>Shipment ID:</b> 123456789</p>
+              <p><b>Shipment Date:</b> 11/01/2018</p>
+              <p><b>Receiver:</b> John Smith</p>
+              <p><b>Tracking Status:</b> At Shanghai airport waiting for customs clearance.</p>
+            </div>
+          </div>`
+  }
+];
+  
+
+
+class Map extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false
+    }
+  };
+
+  open = (markerIndex) => {
+    this.setState({
+      isOpen: true
+    });
+  }
+
+  close = () => {
+    this.setState({
+      isOpen: false
+    });
+  }
+
+  render() {
+
+    return(
+      <GoogleMap
+        defaultZoom={3}
+        defaultCenter={{ lat: 25.062400, lng: 153.042605 }}
+      >
+      <MarkerClusterer
+        averageCenter
+        enableRetinaIcons
+        gridSize={90}
+      >
+        {
+          markers.map(marker =>
+            <Marker 
+              key={marker.index} 
+              position={{ lat: marker.lat, lng: marker.lng }}
+              options={{ icon: marker.icon }}
+              onClick={() => this.open()}
+            >
+                {this.state.isOpen &&
+                  <InfoWindow onCloseClick={() => this.setState({ isOpen: false })}>
+                    <div>
+                      <h1 id="firstHeading" class="firstHeading">Shanghai Pudong International Airport</h1>
+                      <div id="bodyContent">
+                        <p><b>Shipment ID:</b> 123456789</p>
+                        <p><b>Shipment Date:</b> 11/01/2018</p>
+                        <p><b>Receiver:</b> John Smith</p>
+                        <p><b>Tracking Status:</b> At Shanghai airport waiting for customs clearance.</p>
+                      </div>
+                    </div>
+                  </InfoWindow>
+                }
+            </Marker>
+          )}
+        }
+      </MarkerClusterer>
+    </GoogleMap>
+    );
+  }
+}
+
+const MapComponent = withScriptjs(withGoogleMap(Map));
 
 
 class SplashPage extends React.Component {
   constructor(props) {
-      super(props);
-      this.state = {
-      };
-    }
+    super(props);
+    this.state = {
+    };
+  }
 
   render() {
     const { classes } = this.props;
 
     return (
-      <MyMapComponent 
+      <MapComponent
         googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBRTo3HKFYSImMhL6g2M4cErR8iEHNaod8"
         loadingElement={<div style={{ height: `100%` }} />}
-        containerElement={<div style={{ height: `500px` }} />}
+        containerElement={<div style={{ height: `800px` }} />}
         mapElement={<div style={{ height: `100%` }} />}
       />
     );
