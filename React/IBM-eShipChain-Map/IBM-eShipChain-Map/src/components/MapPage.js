88d4d77754f618904394fc9b49582f8ee29e54bb
yhlong0 <yhlong2@gmail.com>
2018-11-05 22:38:45 -0600

    add more images for markers

--- React/IBM-eShipChain-Map/IBM-eShipChain-Map/src/components/MapPage.js
+++ React/IBM-eShipChain-Map/IBM-eShipChain-Map/src/components/MapPage.js
@@ -6,6 +6,11 @@ import { withStyles } from "@material-ui/core/styles";
 import MarkerClusterer from "react-google-maps/lib/components/addons/MarkerClusterer";
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps";
 import box from '../img/box.png';
+import blinkBox from '../img/box.gif';
+import plane from '../img/air-freight.png';
+import ship from '../img/cargo-ship.png';
+import truck from '../img/delivery-truck.png';
+import redBox from '../img/redbox.png';
 
 
 
@@ -20,18 +25,17 @@ const MyMapComponent = withScriptjs(withGoogleMap((props) =>
     defaultZoom={3}
     defaultCenter={{ lat: 25.062400, lng: 153.042605 }}
   >
-
-
     <MarkerClusterer
       averageCenter
       enableRetinaIcons
       gridSize={90}
     >
-      <Marker position={{ lat: 35.900762, lng: 139.642131 }} />
-      <Marker position={{lat: 45.848175, lng: -108.387897 }} />
-      <Marker position={{lat: 19.192271, lng: -99.373728 }} />
-      <Marker position={{lat: 17.192271, lng: 83.373728 }} />
-      <Marker position={{lat: 34.679271, lng: 135.479528 }} />
+      <Marker position={{ lat: 35.900762, lng: 139.642131 }} options={{ icon: box }} />
+      <Marker position={{lat: 45.848175, lng: -108.387897 }} options={{ icon: blinkBox }} />
+      <Marker position={{lat: 19.192271, lng: -99.373728 }} options={{ icon: plane }} />
+      <Marker position={{lat: 17.192271, lng: 83.373728 }} options={{ icon: ship }} />
+      <Marker position={{lat: 34.679271, lng: 135.479528 }} options={{ icon: truck }} />
+      <Marker position={{lat: 24.679271, lng: 125.479528 }} options={{ icon: redBox }} />
     </MarkerClusterer>
   </GoogleMap>
 ));
