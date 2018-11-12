a0da7eeb96ae24b782431f88425b4db56b010bd4
yhlong0 <yhlong2@gmail.com>
2018-11-12 09:58:47 -0600

    object for one level check

--- JavaScript/checkCondition.js
+++ JavaScript/checkCondition.js
@@ -50,3 +50,22 @@ const onButtonClick = (status) => {
             break
     }
 }
+
+
+
+const actions = {
+    '1': ['processing', 'IndexPage'],
+    '2': ['fail', 'FailPage'],
+    '3': ['fail', 'FailPage'],
+    '4': ['success', 'SuccessPage'],
+    '5': ['cancel', 'CancelPage'],
+    'default': ['other', 'Index'],
+}
+
+const onButtonClick = (status) => {
+    let action = actions[status] || actions['default'],
+        logName = action[0],
+        pageName = action[1]
+    sendLog(logName)
+    jumpTo(pageName)
+}
