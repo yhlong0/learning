9ce2386aede73645772f92b48453b7629fa96b4f
Hailong Yang <yhlong2@gmail.com>
2019-03-31 11:34:59 -0400

    Update closure.js

--- JavaScript/closure.js
+++ JavaScript/closure.js
@@ -1,4 +1,5 @@
-# What is a closure?
-# A closure is a feature in JavaScript where an inner function has access to the outer (enclosing) function’s variables — a scope chain.
-
+/* 
+* What is a closure?
+* A closure is a feature in JavaScript where an inner function has access to the outer (enclosing) function’s variables — a scope chain.
+*/
 
