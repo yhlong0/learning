b8f55bb12c6144c662baf24a2d2a9136d52d5ecb
yhlong0 <yhlong2@gmail.comn>
2019-04-02 21:09:44 -0400

    add children render props

--- React/RenderProps.js
+++ React/RenderProps.js
@@ -1,48 +1,67 @@
 class Toggle extends Component {
   state = {
-    on: false,
-  }
-  
+    on: false
+  };
+
   toggle = () => {
     this.setState({
       on: !this.state.on
-    })
-  }
-  
+    });
+  };
+
   render() {
     return (
       <div>
         {this.state.on && this.props.children}
         <button onClick={this.toggle}>Show/Hide</button>
       </div>
-    )
+    );
   }
 }
 
 class RenderPropsToggle extends Component {
   state = {
-    on: false,
-  }
-  
+    on: false
+  };
+
   toggle = () => {
     this.setState({
       on: !this.state.on
-    })
-  }
-  
+    });
+  };
+
   render() {
-    const { render } = this.props
+    const { render } = this.props;
     return (
       <div>
         {render({
           on: this.state.on,
-          toggle: this.toggle,
+          toggle: this.toggle
         })}
       </div>
-    )
+    );
   }
 }
 
+class RPChildrenToggle extends Component {
+  state = {
+    on: false
+  };
+
+  toggle = () => {
+    this.setState({
+      on: !this.state.on
+    });
+  };
+
+  render() {
+    const { children } = this.props;
+    return children({
+      on: this.state.on,
+      toggle: this.toggle
+    });
+  }
+}
 
 class App extends Component {
   render() {
@@ -51,13 +70,23 @@ class App extends Component {
         <Toggle>
           <h1>This goes to props.children</h1>
         </Toggle>
-        <RenderPropsToggle render={({on, toggle}) => (
-          <div>
-            {on && <h1>Show Me</h1>}
-            <button onClick={toggle}>Show/Hide</button>
-          </div>
-        )} />
+        <RenderPropsToggle
+          render={({ on, toggle }) => (
+            <div>
+              {on && <h1>Show Me</h1>}
+              <button onClick={toggle}>Show/Hide</button>
+            </div>
+          )}
+        />
+        <RPChildrenToggle>
+          {({ on, toggle }) => (
+            <div>
+              {on && <h1>Show Me</h1>}
+              <button onClick={toggle}>Show/Hide</button>
+            </div>
+          )}
+        </RPChildrenToggle>
       </div>
-    )
+    );
   }
 }
