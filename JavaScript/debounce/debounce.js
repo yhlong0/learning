e13d0d16db63ceb4ad80ee5c0e5907ecd700a824
hyang682 <Hailong_Yang@comcast.com>
2019-09-03 22:01:26 -0400

    fix this point to window

--- JavaScript/debounce/debounce.js
+++ JavaScript/debounce/debounce.js
@@ -4,13 +4,18 @@ let container = document.getElementById("container");
 function debounce(func, wait) {
   let timeout;
   return function() {
+    const context = this;
     clearTimeout(timeout);
-    timeout = setTimeout(func, wait);
+    timeout = setTimeout(function() {
+      func.apply(context);
+    }, wait);
   };
 }
 
 function getUserAction() {
+  console.log("This:", this);
   container.innerHTML = count++;
 }
 
 container.onmousemove = debounce(getUserAction, 1000);
+// container.onmousemove = getUserAction;
