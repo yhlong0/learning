7f8d1a370b20c07de37a001080010966bb13195e
Hailong Yang <yhlong2@gmail.com>
2019-12-07 23:45:03 -0500

    Update useReducer.js

--- React/Hooks/useReducer.js
+++ React/Hooks/useReducer.js
@@ -1,1 +1,11 @@
-// Use Reducer
+import React from 'react'
+
+export default function Login() {
+  return (
+    <form onSubmit={onSubmit}>
+      <input type="text" placeholder="username" />
+      <input type="password" placeholder="password" />
+      <button type="submit">Log In </button>
+    </form>
+  )
+}
