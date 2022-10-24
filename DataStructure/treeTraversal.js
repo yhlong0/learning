1a9cb549c5c0ec9e3a91daaea46b35af175acac1
HelloWorld <yhlong2@gmail.com>
2022-10-24 16:02:35 -0400

    Update treeTraversal.js

--- DataStructure/treeTraversal.js
+++ DataStructure/treeTraversal.js
@@ -47,6 +47,29 @@ const inorderTraversal = (root) => {
     return [ ...inorderTraversal(root.left), root.val, ...inorderTraversal(root.right)]
 };
 
+const inorderTraversal = function(root) {
+    if(root === null) return []
+    
+    let result = []
+    let stack = []
+    let current = root
+    
+    while(current !== null || stack.length > 0) {
+        if(current !== null) {
+            stack.push(current)
+            // result.push(current)
+            current = current.left
+        } else {
+            current = stack.pop()
+            result.push(current.val)
+            current = current.right
+        }
+    
+    }    
+    return result
+    
+};
+
 const postorderTraversal = (root) => {
     if(root === null || root.val === undefined) {
         return []
